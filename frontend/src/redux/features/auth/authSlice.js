f16ecf9a4580607c1c87aab5d31b30479b6e721c
Sangar <sangarsmart16@gmail.com>
2024-04-29 20:50:26 +0530

    done

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -15,10 +15,10 @@ const authSlice = createSlice({
       const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
       localStorage.setItem("expirationTime", expirationTime);
     },
-  },
-  logout: (state) => {
-    state.userInfo = null;
-    localStorage.clear();
+    logout: (state) => {
+      state.userInfo = null;
+      localStorage.clear();
+    },
   },
 });
 
