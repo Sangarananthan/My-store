32552e817348db5dd4da4b18dcaf96eb09aefb65
Sangar <sangarsmart16@gmail.com>
2024-04-30 05:48:58 +0530

    registeration page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../src/redux/features/store.js";
 import Login from "./pages/auth/Login.jsx";
+import Register from "./pages/auth/Register.jsx";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Route>
   )
 );
