009649f7522f32bdb141737623e07d555df4b7e9
Sangar <sangarsmart16@gmail.com>
2024-05-03 13:16:55 +0530

    file upload backend

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import { connectDb } from "./config/db.js";
 import userRoutes from "./router/userRoutes.js";
 import categoryRoutes from "./router/categoryRoutes.js";
 import productRoutes from "./router/productRoutes.js";
+import uploadRoutes from "./router/uploadRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -23,5 +24,8 @@ app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 app.listen(port, () => console.log(`server running on port ${port}`));
